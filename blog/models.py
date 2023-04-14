a93d6b1f9a9fa18c4dfb70d6919319e32d95f9ae
Andrey7777777777 <117460052+Andrey7777777777@users.noreply.github.com>
2023-04-14 12:38:12 +0300

    move queries into QuerySet methods.

--- blog/models.py
+++ blog/models.py
@@ -10,12 +10,27 @@ class PostQuerySet(models.QuerySet):
         posts_at_year = self.filter(published_at__year=year).order_by('published_at')
         return posts_at_year
 
+    def popular(self):
+        popular_posts = self.annotate(likes_count=Count('likes')).order_by('-likes_count')
+        return popular_posts
+
+    def fetch_with_comments_count(self):
+        most_popular_posts = list(self)
+        most_popular_posts_ids = [post.id for post in most_popular_posts]
+        posts_with_comments = Post.objects.filter(id__in=most_popular_posts_ids).annotate(
+            comments_count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+        for post in most_popular_posts:
+            post.comments_count = count_for_id[post.id]
+        return most_popular_posts
+
 
 class TagQuerySet(models.QuerySet):
 
     def popular(self):
-        posts_at_year = self.annotate(Count('posts')).order_by('-posts__count')
-        return posts_at_year
+        popular_tags = self.annotate(Count('posts')).order_by('-posts__count')
+        return popular_tags
 
 
 class Post(models.Model):
